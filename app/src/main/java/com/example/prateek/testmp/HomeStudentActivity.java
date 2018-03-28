5927b474a9a4a24d569d2cda7447913cf597dff9
Vaidikk <vedik.khandelwal@gmail.com>
2018-03-28 11:25:20 +0530

    Includes ongoingTestActivity

---
+++ app/src/main/java/com/example/prateek/testmp/HomeStudentActivity.java
@@ -0,0 +1,97 @@
+package com.example.prateek.testmp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class HomeStudentActivity extends AppCompatActivity {
+
+    ListView testListView;
+    private DatabaseReference mDatabase;
+
+    ArrayList<String> testName=new ArrayList<>();
+    ArrayList<String> testUID=new ArrayList<>();
+    ArrayList<String> fullTestUID = new ArrayList<>();
+
+    String test_uid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_student);
+        this.setTitle("Home Student");
+
+        testListView=findViewById(R.id.TestListView);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
+        try{
+
+
+            mDatabase.child("tests").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                        test_uid=ds.getKey().toString();
+                        fullTestUID.add(test_uid);
+                        String[] strings=test_uid.split("\\*");
+
+                        Log.i("TestName===",strings[0]);
+                        Log.i("TestName===",strings[1]);
+
+                        testName.add(strings[0]);
+                        testUID.add(strings[1]);
+
+                    }
+
+                    ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,testName);
+
+                    testListView.setAdapter(listAdapter);
+
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        onTestClick();
+
+    }
+
+    private void onTestClick() {
+
+        testListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Intent intent = new Intent(HomeStudentActivity.this, OngoingTestActivity.class);
+                intent.putExtra("test_uid", fullTestUID.get(position));
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/prateek/testmp/OngoingTestActivity.java
@@ -0,0 +1,125 @@
+package com.example.prateek.testmp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class OngoingTestActivity extends AppCompatActivity {
+
+    TextView textViewQuestion;
+    RadioGroup radioGroupOptions;
+    RadioButton radioButtonOptionA, radioButtonOptionB, radioButtonOptionC, radioButtonOptionD;
+    Button buttonNext;
+
+    String test_uid;
+
+    int questionNo = 0;
+
+    ArrayList<TestQuestionDetails> testQuestionDetailsArrayList = new ArrayList<>();
+
+    public FirebaseAuth firebaseAuth;
+
+    private DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ongoing_test);
+
+        XMLReferences();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        Intent intent = getIntent();
+        test_uid = intent.getStringExtra("test_uid");
+        Log.i("####Value===", test_uid);
+
+        getQuestions();
+
+//        displayQuestion();
+    }
+
+    private void displayQuestion() {
+
+        TestQuestionDetails testQuestionDetails = testQuestionDetailsArrayList.get(questionNo);
+        textViewQuestion.append(testQuestionDetails.question);
+        radioButtonOptionA.setText("A. " + testQuestionDetails.a);
+        radioButtonOptionB.setText("B. " + testQuestionDetails.b);
+        radioButtonOptionC.setText("C. " + testQuestionDetails.c);
+        radioButtonOptionD.setText("D. " + testQuestionDetails.d);
+
+    }
+
+    private void getQuestions() {
+
+
+        try {
+            mDatabase.child("tests").child(test_uid).child("testQuestionDetails").addValueEventListener(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                        TestQuestionDetails testQuestionDetails = ds.getValue(TestQuestionDetails.class);
+
+                        ds.child("");
+                        try {
+
+                            testQuestionDetailsArrayList.add(testQuestionDetails);
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    Log.i("size of array: ", testQuestionDetailsArrayList.size()+"");
+                    displayQuestion();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+        catch (Exception e){e.printStackTrace();}
+    }
+
+
+    public void onNextButtonClicked(View view){
+        questionNo++;
+        if(questionNo < testQuestionDetailsArrayList.size()) {
+            Log.i("### Question No.: ", questionNo + "");
+            textViewQuestion.setText("");
+            displayQuestion();
+        }
+    }
+
+    private void XMLReferences() {
+
+        textViewQuestion = findViewById(R.id.textViewQuestion);
+        radioGroupOptions = findViewById(R.id.radioGroupOptions);
+        radioButtonOptionA = findViewById(R.id.radioButtonOptionA);
+        radioButtonOptionB = findViewById(R.id.radioButtonOptionB);
+        radioButtonOptionC = findViewById(R.id.radioButtonOptionC);
+        radioButtonOptionD = findViewById(R.id.radioButtonOptionD);
+        buttonNext = findViewById(R.id.buttonNext);
+    }
+}
